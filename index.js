38b5f50d45b63f9e74404f31762cb2112409f92c
ViniciusPilati33333 <vini1718a@gmail.com>
2024-07-18 21:45:36 -0300

    carrosel básico, completo

---
+++ index.js
@@ -0,0 +1,32 @@
+const imgs = [
+  "https://sudotec.org.br/upload/projetos/p_89.jpg?202407172048",
+  "https://sudotec.org.br/upload/projetos/p_92.jpg?202407172048",
+  "https://sudotec.org.br/upload/projetos/p_93.jpg?202407172048",
+];
+
+let indexCarrossel = 0;
+
+function voltaImagem() {
+  const imgCarrosel = document.getElementById("imagem-carrosel");
+  indexCarrossel--;
+
+  if (indexCarrossel < 0) {
+    indexCarrossel = imgs.length - 1;
+  }
+
+  imgCarrosel.setAttribute(`src`, imgs[indexCarrossel]);
+}
+
+function passaImagem() {
+  const imgCarrosel = document.getElementById("imagem-carrosel");
+  indexCarrossel++;
+
+  if (indexCarrossel >= imgs.length) {
+    indexCarrossel = 0;
+  }
+  imgCarrosel.setAttribute(`src`, imgs[indexCarrossel]);
+
+  setInterval(() => {
+    passaImagem();
+  }, 3000);
+}
